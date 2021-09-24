199449f51c7e8bdb00c3a19aa1e79055274fe4c1
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-09-24 16:04:05 -0700

    Implemented add and contains

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  /*  if item is not in storage already
+   *    push to storage array
+   */
+  if (!this._storage.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  /*  note: can compare using === because only expecting string type
+   *  for each item in storage
+   *    if (storage item === item)
+   *      retun true
+   *  return false
+   */
+
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
