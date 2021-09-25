5d095f9705a6b0ba388d3d7ef1500d3f6204f0bf
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-09-25 12:16:10 -0700

    Implemented new test for graphSpec

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -68,4 +68,11 @@ describe('graph', function() {
     expect(graph.hasEdge(3, 5)).to.equal(true);
     expect(graph.hasEdge(5, 5)).to.equal(true);
   });
+
+
+  it('[NEW TEST] should not contain values that were not inserted', function() {
+    graph.addNode(1);
+    expect(graph.contains(2)).to.equal(false);
+  });
+
 });
