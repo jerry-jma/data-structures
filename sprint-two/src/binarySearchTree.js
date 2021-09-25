6eed7e9ac2f590f59543b69ce9c5bd1ca582ce4b
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-09-25 11:40:22 -0700

    Added time complexity in comments

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -54,4 +54,12 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+-----------insert-------------------
+O(log(n))
+
+-----------contains-----------------
+O(log(n))
+
+-----------depthFirstLog------------
+O(n)
  */
