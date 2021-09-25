a7a79087be371fa9cd57f150fbd76c84ccb6591b
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-09-25 10:48:17 -0700

    Implemented constructor and shell for methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,22 @@
 var BinarySearchTree = function(value) {
+  var instance = Object.create(BinarySearchTree.prototype);
+  instance.value = value;
+  instance.left = null;
+  instance.right = null;
+  return instance;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
